d8ec724464ce99e1e810285fff49f41c5d04a5c0
Johannes Schumeth <johannes.schumeth@gmail.com>
2024-05-31 15:19:41 +0200

    Finish

--- ImageMagick.Test/Program.cs
+++ ImageMagick.Test/Program.cs
@@ -4,11 +4,18 @@ using ImageMagick.Test;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+ResourceLimits.Memory = 1073741824;
+ResourceLimits.MaxMemoryRequest = 20000000;//20 Megabyte
+ResourceLimits.Throttle = 50;
+ResourceLimits.Thread = 1;
+ResourceLimits.Time = 120;
+
 app.MapGet("/", async () =>
 {
     var imageProcessor = new ImageProcessor();
     var fileStream = new FileStream("Assets/DSCF7482.jpg", FileMode.Open);
-    var compressed = imageProcessor.Compress(fileStream, ".jpg", 75, MagickFormat.WebP, 1080, 1920, true);
+    //NOTE: add height parameter here to make it work
+    var compressed = imageProcessor.Compress(fileStream, ".jpg", 75, MagickFormat.WebP, 1920, useWaterMark: false);
     return await imageProcessor.SaveCompressed(compressed);
 });
 
